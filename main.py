65d91809960e47fa7d593c469cacd998a9b02ae5
danila <danilalapin707@gmail.com>
2024-11-22 18:09:59 +0300

    Поменяли логирование

--- main.py
+++ main.py
@@ -29,6 +29,11 @@ load_dotenv()
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
+logging.getLogger("httpx").setLevel(logging.WARNING)
+
+logger = logging.getLogger(__name__)
+
+
 
 if __name__ == "__main__":
     TOKEN = os.getenv('TOKEN')
